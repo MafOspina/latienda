5487206ba9332e6effe26b8114aedcb964c12c2d
Your Name <you@example.com>
2022-05-12 17:30:35 -0500

    layout y materialize

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+
+    <link rel="stylesheet" href="{{ asset('materialize/css/materialize.css')}}"/>    
+</head>
+<body>
+
+    <nav class="cyan darken-3">
+        <div class="nav-wrapper" >
+        <a href="#" class="brand-logo">La Tienda</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Productos</a></li>
+            <li><a href="badges.html">Pedidos</a></li>
+        </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('contenido')
+    </div>
+
+    <script src="{{asset ('materialize/js/materialize.js') }}"></script>
+        
+</body>
+</html>
---
+++ resources/views/productos/new.blade.php
@@ -0,0 +1,55 @@
+<!--para que pueda inlcuirse en la masterpage-->
+@extends('layouts.menu')
+
+@section('contenido')
+<div class="row">
+    <h1 class="blue-grey-text text-darken-2">Nuevo producto</h1>
+
+</div>
+
+<div class="row">
+    <form class="col s12" action="">
+        <div class="row">
+            <div class="input-field col s6">
+
+            <input placeholder="Nombre del producto" type="text" id="nombre" name="nombre">
+            <label for="nombre">Nombre</label>
+
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field col s6">
+                <textarea name="desc" id="desc" class="materialize-textarea"></textarea>
+                <label for="desc">Descripción</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field col s6">
+                <input placeholder="Precio" type="number" id="precio" name="precio">
+                <label for="precio">Precio</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="file-field input-field col s6">
+                <div class="btn">
+                    <span>Imagen producto</span>
+                    <input type="file" name="imagen">
+                </div>
+
+                <div class="file-path wrapper">
+                    <input type="text" class="file-path">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <button class="btn waves-effect waves-light" type="submit" name="action">Guardar
+            </button>
+        </div>
+    </form>
+</div>
+@endsection
